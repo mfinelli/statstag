4951e7b16ece31d690129b3cb6b3108703dbc686
Mario Finelli <mario@finel.li>
2023-06-05 22:08:15 +0200

    Fix style

--- entry.js
+++ entry.js
@@ -1,10 +1,10 @@
 // https://github.com/sveltejs/kit/issues/6841#issuecomment-1330555730
-import {server as app} from './index.js'
+import { server as app } from './index.js';
 
 function gracefulShutdown() {
-	console.log("Shutting down...")
-	app.server.close()
+  console.log('Shutting down...');
+  app.server.close();
 }
 
-process.on("SIGINT", gracefulShutdown)
-process.on("SIGTERM", gracefulShutdown)
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
