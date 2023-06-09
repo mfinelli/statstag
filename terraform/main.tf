ed57ba864703f8ea204c26020dc8ba2804276dbc
Mario Finelli <mario@finel.li>
2023-06-09 22:50:15 +0200

    Create machine

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,30 @@ resource "fly_ip" "v6" {
   app  = fly_app.prod.name
   type = "v6"
 }
+
+resource "fly_machine" "app" {
+  app      = fly_app.prod.name
+  region   = "iad" # nova
+  name     = "app"
+  image    = "mfinelli/statstag:latest"
+  cpus     = 1
+  memorymb = 256
+
+  services = [
+    {
+      protocol      = "tcp"
+      internal_port = 3000
+
+      ports = [
+        {
+          port     = 443
+          handlers = ["tls", "http"]
+        },
+        {
+          port     = 80
+          handlers = ["http"]
+        }
+      ]
+    },
+  ]
+}
