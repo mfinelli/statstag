1ffb4505393fd0fc2d84bd4774598a67f46e000f
Mario Finelli <mario@finel.li>
2023-06-10 11:03:23 +0200

    Add custom domain

---
+++ terraform/dns.tf
@@ -0,0 +1,33 @@
+data "cloudflare_zone" "statstag" {
+  account_id = var.CLOUDFLARE_ACCOUNT_ID
+  name       = "statstag.com"
+}
+
+resource "cloudflare_record" "apex" {
+  zone_id = data.cloudflare_zone.statstag.id
+  name    = data.cloudflare_zone.statstag.zone
+  type    = "AAAA"
+  ttl     = 1
+  value   = fly_ip.v6.address
+}
+
+resource "fly_cert" "statstag" {
+  depends_on = [cloudflare_record.apex]
+  app        = fly_app.prod.name
+  hostname   = data.cloudflare_zone.statstag.name
+}
+
+resource "cloudflare_record" "verification" {
+  zone_id = data.cloudflare_zone.statstag.id
+
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+  value   = fly_cert.statstag.dnsvalidationtarget
+
+  name = replace(
+    fly_cert.statstag.dnsvalidationhostname,
+    ".${cloudflare_zone.statstag.name}",
+    ""
+  )
+}
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -10,6 +10,11 @@ terraform {
   }
 
   required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.7"
+    }
+
     fly = {
       source  = "fly-apps/fly"
       version = "~> 0.0"
@@ -19,4 +24,10 @@ terraform {
 
 provider "fly" {
   fly_http_endpoint = "api.machines.dev"
+}
+
+provider "cloudflare" {}
+
+variable "CLOUDFLARE_ACCOUNT_ID" {
+  type = string
 }
