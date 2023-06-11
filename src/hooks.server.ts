b1d22ab40f34736915033c863121ac6549599a91
Mario Finelli <mario@finel.li>
2023-06-11 22:44:28 +0200

    Add back handle type

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
-// import type Handle from '@sveltejs/kit';
+import type { Handle } from '@sveltejs/kit';
 import { pool } from '$lib/server/db';
 
-export async function handle({ event, resolve }) {
+export const handle: Handle = async ({ event, resolve }) => {
   event.locals.db = pool;
   return await resolve(event);
 }
