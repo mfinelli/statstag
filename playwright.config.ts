72ffbc72c2a0448933de84b46520b8e09a8b177f
Mario Finelli <mario@finel.li>
2023-06-20 23:39:48 +0200

    Run testing on different browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,4 @@
+import { devices } from '@playwright/test';
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
@@ -8,5 +9,38 @@ const config: PlaywrightTestConfig = {
   testDir: 'tests',
   testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 };
+
+if (process.env.CI) {
+  config.projects = [
+    {
+      name: 'Desktop Chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'Desktop Firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'Desktop Safari',
+      use: { ...devices['Desktop Safari'] },
+    },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
+    {
+      name: 'Microsoft Edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    },
+  ];
+}
 
 export default config;
