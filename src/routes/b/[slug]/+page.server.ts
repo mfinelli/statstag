9ab1f991ad04e5ef032df69f4c58f059bc2e04f1
Mario Finelli <mario@finel.li>
2023-06-24 13:53:38 +0200

    Add initial leaderboard view

---
+++ src/routes/b/[slug]/+page.server.ts
@@ -0,0 +1,30 @@
+import { redirect } from '@sveltejs/kit';
+import { z } from 'zod';
+import { sql } from '$lib/server/types';
+import type PageServerLoad from './$types';
+
+export async function load({ locals, params }): PageServerLoad {
+  let leaderboard;
+  let scores;
+
+  await locals.db.connect(async (c) => {
+    return await c.transaction(async (txn) => {
+      leaderboard = await txn.maybeOne(sql.typeAlias('leaderboard')`SELECT
+        id, name, slug FROM leaderboards WHERE slug = ${params.slug} LIMIT 1;`);
+
+      if (leaderboard === null) {
+        throw redirect(307, '/not-found');
+      }
+
+      scores = await txn.any(sql.typeAlias('score')`SELECT id, label, score
+        FROM scores WHERE leaderboard_id = ${leaderboard.id}`);
+    });
+  });
+
+  return {
+    id: (leaderboard === undefined) ? 0 : leaderboard['id'],
+    name: (leaderboard === undefined) ? '' : leaderboard['name'],
+    slug: (leaderboard === undefined) ? '' : leaderboard['slug'],
+    scores: scores,
+  };
+}
---
+++ src/routes/b/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type PageData from './$types';
+  export let data: PageData;
+</script>
+
+<h1>{data.name} ({data.slug})</h1>
+{#each data.scores as score}
+  <p>{score.label}: {score.score}</p>
+{/each}
